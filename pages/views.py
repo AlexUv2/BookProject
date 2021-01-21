8aa57bcc9531917aa1746a577606caea7bfe91bf
AlexUv <sashauva002@gmail.com>
2021-01-21 02:33:47 +0200

    Initial commit

---
+++ pages/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail, get_connection
+from .models import Page
+from .forms import ContactForm
+
+# def index(request):
+#     # return HttpResponse("<h1>The Meandco Homepage</h1>")
+#     return render(request, 'base.html')
+
+
+def index(request, pagename):
+    pagename = '/' + pagename
+    #pg = Page.objects.get(permalink=pagename)
+    pg = get_object_or_404(Page, permalink=pagename)
+    context = {
+        'title': pg.title,
+        'content': pg.body_text,
+        'last_updated': pg.update_date,
+        'page_list': Page.objects.all(),
+    }
+    return render(request, 'pages/page.html', context)
+
+
+def contact(request):
+    submitted = False
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            # False
+            con = get_connection('django.core.mail.backends.console.EmailBackend')
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                cd.get('email', 'noreply@example.com'),
+                ['kovalartem819@gmail.com'],
+                connection=con
+            )
+
+            return HttpResponseRedirect('/contact?submitted=True')
+    else:
+        form = ContactForm()
+        if 'submitted' in request.GET:
+            submitted = True
+
+    return render(request, 'pages/contact.html', {'form': form, 'page_list': Page.objects.all(), 'submitted': submitted})
